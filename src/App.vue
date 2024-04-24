be923ed24270b432c07a015705aef8f56fa44954
kanekoyuya <kaneko@otani-trading.com>
2024-04-25 08:36:02 +0900

    fourth commit

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,31 @@
 <script setup lang="ts">
-import {ref, computed} from "vue";
-import OneMember from "./components/OneMember.vue";
+import { provide, ref} from "vue";
+import Input from "./components/Input.vue";
+import Select from "./components/Select.vue";
+import Radio from "./components/Radio.vue";
 
+const CurrentComp = ref(Input)
 
-//会員リストデータを用意。
-const memberListInit = new Map<number, Member>();
-memberListInit.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特典あり。"});
-memberListInit.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});
-const memberList = ref(memberListInit);
+const CurrentCompName = ref("Input")
 
-//会員リスト内の全会員のポイント合計の算出プロパティ。
-const totalPoints = computed(
-	(): number => {
-		let total = 0;
-		for(const member of memberList.value.values()) {
-			total += member.points;
-		}
-		return total;
+const compList = [Input,Radio,Select]
+const compNameList: string[] = ["Input","Radio","Select"]
+
+let CurrentCompIndex = 0
+const switchComp = (): void => {
+	CurrentCompIndex++
+
+	if(CurrentCompIndex >= 3){
+		CurrentCompIndex = 0
 	}
-);
 
-const onIncrementPoint = (id:number): void => {
-	const member = memberList.value.get(id)
-	if (member != undefined) {
-		member.points++
-	}
-}
+	CurrentComp.value = compList[CurrentCompIndex]
 
-//会員情報インターフェース。
-interface Member {
-	id: number;
-	name: string;
-	email: string;
-	points: number;
-	note?: string;
+	CurrentCompName.value = compNameList[CurrentCompIndex]
+
 }
 </script>
 
 <template>
-	<section>
-		<h1>会員リスト</h1>
-		<p>全会員の保有ポイントの合計: {{totalPoints}}</p>
-		<OneMember
-			v-for="[id, member] in memberList"
-			v-bind:key="id"
-			v-bind:id="id"
-			v-bind:name="member.name"
-			v-bind:email="member.email"
-			v-bind:points="member.points"
-			v-bind:note="member.note"
-			v-on:incrementPoint="onIncrementPoint"/>
-	</section>
+	
 </template>
--- src/components/OneMember.vue
+++ src/components/OneMember.vue
@@ -1,60 +1,57 @@
 <script setup lang="ts">
-import {ref, computed} from "vue";
+import {computed, inject} from "vue";
+import type {Member} from "../interfaces";
 
 //Propsインターフェースの定義。
 interface Props {
 	id: number;
-	name: string;
-	email: string;
-	points: number;
-	note?: string;
 }
-
-const props = defineProps<Props>()
-
-interface Emits {
-    (event: "incrementPoint",id: number): void
-}
-const emit = defineEmits<Emits>()
-
+//Propsオブジェクトの設定。
+const props = defineProps<Props>();
+//会員情報リストをインジェクト。
+const memberList = inject("memberList") as Map<number, Member>;
+//該当する会員情報の取得。
+const member = computed(
+	(): Member => {
+		return memberList.get(props.id) as Member;
+	}
+);
+//Propsのnoteを加工する算出プロパティ。
 const localNote = computed(
 	(): string => {
-		let localNote = props.note;
+		let localNote = member.value.note;
 		if(localNote == undefined) {
 			localNote = "--";
 		}
 		return localNote;
 	}
 );
-
-const pointUp = (): void => {
-	emit("incrementPoint",props.id)
-}
 </script>
 
 <template>
 	<section class="box">
-		<h4>{{name}}さんの情報</h4>
+		<h4>{{member.name}}さんの情報</h4>
 		<dl>
 			<dt>ID</dt>
 			<dd>{{id}}</dd>
 			<dt>メールアドレス</dt>
-			<dd>{{email}}</dd>
+			<dd>{{member.email}}</dd>
 			<dt>保有ポイント</dt>
-			<dd>{{points}}</dd>
+			<dd>
+				<input type="number" v-model.number="member.points">
+			</dd>
 			<dt>備考</dt>
 			<dd>{{localNote}}</dd>
 		</dl>
-		<button v-on:click="pointUp">ポイント加算</button>
 	</section>
 </template>
 
 <style scoped>
 .box {
 	border: green 1px solid;
-	margin: 30px;
+	margin: 10px;
 }
-dt,dd {
-    color: aqua;
+section{
+	background-color: rgb(112, 27, 60);
 }
 </style>
